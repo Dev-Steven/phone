f6a3ca62cba267808eae0fa1b03966780352d079
Steven <steven.chung96@yahoo.com>
2019-08-07 22:49:41 -0700

    edit

--- Phone.cs
+++ Phone.cs
@@ -1,10 +1,5 @@
 public abstract class Phone 
 {
-    // private string _versionNumber;
-    // private int _batteryPercentage;
-    // private string _carrier;
-    // private string _ringTone;
-
     public string _VersionNumber {get;set;}
     public int _BatteryPercentage {get;set;}
     public string _Carrier {get;set;}
@@ -16,9 +11,6 @@ public abstract class Phone
         _Carrier = carrier;
         _RingTone = ringTone;
 }
-    // abstract method. This method will be implemented by the subclasses
     public abstract void DisplayInfo();
-    // public getters and setters removed for brevity. Please implement them yourself
- 
 }
         
